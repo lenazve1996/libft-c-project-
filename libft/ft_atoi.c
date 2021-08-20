8966374df0d5db0cd83895e10d58a85e18b1dbd6
lenazve1996 <55851043+lenazve1996@users.noreply.github.com>
2021-08-20 19:28:22 +0300

    Add files via upload

---
+++ libft/ft_atoi.c
@@ -0,0 +1,30 @@
+#include "libft.h"
+
+int ft_atoi(const char *str)
+{
+	int sign;
+	long long int count;
+
+	count = 0;
+	sign = 1;
+	while (*str == ' ' || *str == '\t' || *str == '\v' || *str == '\f' || *str == '\r' || *str == '\n')
+		str++;
+	if (*str == '+')
+			str++;
+	else if (*str == '-')
+		{
+			sign = sign * -1;
+			str++;
+		}
+	while (*str)
+	{
+		if ((*str >= '0' && *str <= '9'))
+		{
+			count = (count * 10) + (*str - 48);
+			str++;
+		}
+		else
+			return (count * sign);
+	}
+	return (count * sign);
+}
---
+++ libft/ft_memmove.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	if ((void *)dst == NULL || (void *)src == NULL)
+		return (NULL);
+	char			*tmp_src;
+	char			*tmp_dst;
+	
+	tmp_dst = (char *)dst;
+	tmp_src = (char *)src;
+
+	if (tmp_src < tmp_dst)
+	{
+		tmp_dst = tmp_dst+ len;
+		tmp_src = tmp_src + len;
+		while (len--)
+			*--tmp_dst = *--tmp_src;
+	}
+	else
+	{
+		while(len--)
+			*tmp_dst++ = *tmp_src++;
+	}
+	return(dst);
+}
